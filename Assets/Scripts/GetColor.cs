44ed88d93d18d28d557928ac703ea86a4fd7c9fd
yujun07 <ujun08140@naver.com>
2024-06-19 19:34:32 +0900

    ColorSpoid update
    bug fix & other way script

---
+++ Assets/Scripts/GetColor.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetColor : MonoBehaviour
+{
+    public KeyCode getColorKey = KeyCode.E;
+    public KeyCode applyColorKey = KeyCode.F;
+    
+    public Color currentColor;
+    public Color defaultColor = new Color(128, 128, 128);
+    public bool holdColor = false;
+    private void GetMaterial()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if(Physics.Raycast(ray,out hit))
+        {
+            currentColor = hit.transform.gameObject.GetComponent<MeshRenderer>().material.color;
+            hit.transform.gameObject.GetComponent<MeshRenderer>().material.color = defaultColor;
+            holdColor = true;
+        }
+    }
+    private void ApplyColor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit) && holdColor==true)
+        {
+            hit.transform.gameObject.GetComponent<MeshRenderer>().material.color = currentColor;
+            holdColor=false;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(getColorKey))
+        {
+            GetMaterial();
+        }
+        if(Input.GetKeyDown(applyColorKey))
+        {
+            ApplyColor();
+        }
+    }
+}
