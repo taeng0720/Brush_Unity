c42339e9b6ce212a5bf9cc2df8e1313ed46fc4fa
yujun07 <ujun08140@naver.com>
2024-06-26 20:33:23 +0900

    Update
    color inventory & fps status

--- Assets/Scripts/GetColor.cs
+++ Assets/Scripts/GetColor.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class GetColor : MonoBehaviour
 {
@@ -10,30 +12,67 @@ public class GetColor : MonoBehaviour
     public Color currentColor;
     public Color defaultColor = new Color(128, 128, 128);
     public bool holdColor = false;
+
+    private int selectColor = 1;
+    public Image[] colorsUI;
     private void GetMaterial()
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
-        if(Physics.Raycast(ray,out hit))
+        if(Physics.Raycast(ray,out hit) && hit.transform.gameObject.GetComponent<MeshRenderer>().material.color != defaultColor)
         {
             currentColor = hit.transform.gameObject.GetComponent<MeshRenderer>().material.color;
             hit.transform.gameObject.GetComponent<MeshRenderer>().material.color = defaultColor;
-            holdColor = true;
+            ColorInventory();
         }
+    }
+    private void SelectedColor()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1)) selectColor = 1;
+        if (Input.GetKeyDown(KeyCode.Alpha2)) selectColor = 2;
+        if (Input.GetKeyDown(KeyCode.Alpha3)) selectColor = 3;
     }
     private void ApplyColor()
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
-        if (Physics.Raycast(ray, out hit) && holdColor==true)
+        if (Physics.Raycast(ray, out hit) && colorsUI[selectColor-1].color != defaultColor)
         {
-            hit.transform.gameObject.GetComponent<MeshRenderer>().material.color = currentColor;
+            hit.transform.gameObject.GetComponent<MeshRenderer>().material.color = colorsUI[selectColor-1].color;
+            colorsUI[selectColor - 1].color = defaultColor;
             holdColor=false;
         }
     }
-    // Update is called once per frame
+    private void ColorInventory()
+    {
+        switch (selectColor)
+        {
+            case 1:
+                colorsUI[0].color = currentColor;
+                holdColor = true;
+                break;
+            case 2:
+                colorsUI[1].color = currentColor;
+                holdColor = true;
+                break;
+            case 3:
+                colorsUI[2].color = currentColor;
+                holdColor = true;
+                break;
+        }
+        if (colorsUI[selectColor-1].color != defaultColor) holdColor = false;
+    }
+    
+    private void Start()
+    {
+        for(int i=0; i<colorsUI.Length;i++)
+        {
+            colorsUI[i].color = defaultColor;
+        }
+    }
     void Update()
     {
+        SelectedColor();
         if(Input.GetKeyDown(getColorKey))
         {
             GetMaterial();
