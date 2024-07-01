7903ccfc5934cbccc307d1d17eec82332cac6383
yujun07 <ujun08140@naver.com>
2024-07-01 14:26:28 +0900

    Update
    UI,Game Exit,Continue

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,11 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    private static GameManager instance;
+    public static GameManager instance;
 
+    public Button startBtn;
+    public Button exitBtn;
     private void Awake()
     {
         if(instance == null)
@@ -19,14 +22,53 @@ public class GameManager : MonoBehaviour
             Destroy(gameObject);
         }
     }
+    private void Update()
+    {
+        if (SceneManager.GetActiveScene().name == "MainScene")
+        {
+            gameObject.GetComponent<FpsStatus>().enabled = false;
+        }
+    }
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.name == "MainScene")
+        {
+            // 메인 씬에서 버튼 찾기
+            startBtn = GameObject.Find("Startbtn").GetComponent<Button>();
+            exitBtn = GameObject.Find("Exitbtn").GetComponent<Button>();
+            
+            if (startBtn != null && exitBtn != null)
+            {
+                startBtn.onClick.AddListener(Startbtn);
+                exitBtn.onClick.AddListener(GameExit);
+            }
+        }
+    }
+
     public void Startbtn()
     {
         SceneManager.LoadScene("GameScene");
         gameObject.GetComponent<FpsStatus>().enabled = true;
-        gameObject.GetComponent<Setting>().enabled = true;
         DataManager.Instance.LoadGameData();
     }
     
+    public void GameExit()
+    {
+    #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+    #else
+        Application.Quit();
+    #endif
+    }
     public void Savebtn()
     {
         DataManager.Instance.SaveGameData();
@@ -35,6 +77,7 @@ public class GameManager : MonoBehaviour
     {
         DataManager.Instance.LoadGameData();
     }
+    
     private void OnApplicationQuit()
     {
         DataManager.Instance.SaveGameData();
---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using JetBrains.Annotations;
+
+public class MenuManager : MonoBehaviour
+{
+    public static MenuManager instance;
+
+    public GameObject menuPanel;
+    public bool menuEnabled = false;
+    private GameObject canvas;
+    public Button exitBtn;
+    public Button continueBtn;
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.name == "GameScene")
+        {
+            canvas = GameObject.Find("Canvas");
+            menuPanel = canvas.transform.GetChild(0).gameObject;
+            exitBtn = menuPanel.transform.GetChild(0).gameObject.GetComponent<Button>();
+            continueBtn = menuPanel.transform.GetChild(1).gameObject.GetComponent<Button>();
+            if (exitBtn != null && continueBtn != null)
+            {
+                exitBtn.onClick.AddListener(Exitbtn);
+                continueBtn.onClick.AddListener(Continue);
+            }
+        }
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(SceneManager.GetActiveScene().name == "GameScene")
+        {
+            if (Input.GetKeyDown(KeyCode.Escape) && menuEnabled == false)
+            {
+                menuPanel.SetActive(true);
+                menuEnabled = true;
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+            }
+            else if (Input.GetKeyDown(KeyCode.Escape) && menuEnabled == true)
+            {
+                Continue();
+            }
+        }
+    }
+
+    public void Exitbtn()
+    {
+        menuEnabled = false;
+        SceneManager.LoadSceneAsync("MainScene", LoadSceneMode.Single);
+        DataManager.Instance.SaveGameData();
+    }
+    public void Continue()
+    {
+        menuPanel.SetActive(false);
+        menuEnabled = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+}
