0d0fe5c658675b4e0dc00357238d1a66a2034160
taeng <xodn0720@naver.com>
2024-09-30 11:18:26 +0900

    update

---
+++ Assets/Scripts/Anim_Prograss.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class Anim_Prograss : MonoBehaviour
+{
+    public GameObject[] tiles;
+    public ProgressManager progressManager;
+    void Start()
+    {
+
+    }
+}
